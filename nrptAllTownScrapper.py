404af67d2582a74621b370b968a5b39701cfb7a1
BenyDZ <patdenzoungany@gmail.com>
2019-02-10 06:01:57 +0100

    initial version

---
+++ nrptAllTownScrapper.py
@@ -0,0 +1,188 @@
+#-------------------------------------------------------------------------------
+# Name:        Email Scraper
+# Purpose:      Create a that find email address by the domain name
+#
+# Author:      Beny-DZIENGUE
+#
+# Created:     03/01/2019
+# Copyright:   (c) Beny-DZIENGUE 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#import needed object
+import re
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
+import http.client
+import socket
+from bs4 import BeautifulSoup
+
+def getPage(page):
+    try:
+        # always set a timeout when you connect to an external server
+        url =  "nrpt.co.uk"
+        #coonect to the http server, return the object connection of type http.client.HTTPConnection
+        connection = http.client.HTTPSConnection(url, timeout=10)
+        #send request to the server, return an object of type NoneTYpe
+        connection.request("GET", page)
+        #get response from the server, return the object response of type http.client.HTTPResponse
+        response = connection.getresponse()
+        #read and reurn the response
+        return response.read()
+    except socket.timeout:
+        # in a real world scenario you would probably do stuff if the
+        # socket goes into timeout
+        pass
+
+def getTownName(townUrl,townNames):
+    #condition to read each element of townUrl
+    for url in townUrl:
+        #add url text in townNames
+        townNames.append(url.text)
+        #return townNames
+    return townNames
+
+def getTownUrl():
+    page = "/town/index.htm"
+    #get the result of the research, return the object result of type bytes
+    result = getPage(page)
+    #create the object that will contain all town href, return the object townHref of type list
+    townHref = []
+    #create the object that will contain all townName, return the object townName of type list
+    townNames = []
+    #create a beautifumsoup object, return the object soup of type bs4.BeautifulSoup
+    soup = BeautifulSoup(result,features="html.parser")
+    #get result container, return the object resultContainer of type list
+    resultContainer = soup.find('div', class_="content").find_all('p')
+    #initialise the object counter to 7
+    counter = 7
+    #condition to verifie if counter is less than 10
+    while counter < 10:
+        #get all link in the current container, return the object townUrl of type list
+        townUrl = resultContainer[counter].find_all('a')
+        #condition to read each element of townUrl
+        for url in townUrl:
+            #add all href in townHref
+            townHref.append(url.get('href'))
+        #initialise the object townName with the result of the function getTownName, return the object townName of type list
+        townName = getTownName(townUrl,townNames)
+        #increment counter to 1
+        counter += 1
+    #initialise the object townNameAndHref with townName and townHref, return the object townName and townHref of type list
+    towNameAndHref = [townName,townHref]
+    #return towNameAndHref
+    return towNameAndHref
+
+def getPaginationLinks ():
+    #get the result of the research, return the object result of type bytes
+    result = getPage(townHref)
+    #create a beautifumsoup object, return the object soup of type bs4.BeautifulSoup
+    soup = BeautifulSoup(result,features="html.parser")
+    #create the object that will contain the href of all links, return the object paginationLinks
+    paginationLinks = []
+    try :
+        #get all link of the pagination, return the object pagination of type bs4.element.ResultSet
+        pagination = soup.find('div', class_="pager").find_all('a')
+        #condition to read each element of pagination
+        for link in pagination:
+            if len(pagination) == 1:
+                paginationLinks.append(link.get("href"))
+            #condition to verify if the actual element is the last
+            elif len(pagination) > 1 and link == pagination[-1]:
+                #pass
+                pass
+            else:
+                #add the href of the link in paginationLinks
+                paginationLinks.append(link.get("href"))
+    except :
+        pass
+
+    #return paginationLinks
+    return paginationLinks
+
+def getTrainersName(soup,trainersName):
+    #get all h2 Tag, return the object h2Tag of type list
+    h2Tag = soup.find('ul', class_="searchresults").find_all('h2')
+    #condition to read each element of h2Tag
+    for h2 in h2Tag:
+        #add all link text in trainersName
+        trainersName.append(h2.find('a').text)
+    #return trainersName
+    return trainersName
+
+def getTrainersUrl(pageList):
+    #create the object that will contain all trainersUrl href, return the object trainersHref of type list
+    trainersHref = []
+    #create the object that will contain all traners names, return the object trainersName of type list
+    trainersName = []
+    #excecute 7 instruction in same time
+    with PoolExecutor(max_workers=7) as executor:
+        #get the result of the research, return the object sourcePage of type bytes
+        for sourcePage in executor.map(getPage, pageList):
+            #create a beautifulsoup object, return the object soup of type bs4.BeautifulSoup
+            soup = BeautifulSoup(sourcePage,features="html.parser")
+            #get all trainers url, return the object trainersUrl of type list
+            trainersUrl = soup.find_all("a", class_="wtrk-click")
+            for link in trainersUrl:
+                if link.get("href") not in trainersHref:
+                    trainersHref.append(link.get("href"))
+            trainersName = getTrainersName(soup,trainersName)
+
+    #create the object that will contain trainersName and trainerHref
+    trainerInfo = [trainersHref,trainersName]
+    return trainerInfo
+
+def getTrainersWebsite(trainerHref):
+    #create the object that will contain all trainers Website url, return the object trainerWebsite of type list
+    trainerWebsite = []
+    #excecute 7 instruction in same time
+    with PoolExecutor(max_workers=7) as executor:
+        #get the result of the research, return the object sourcePage of type bytes
+        for sourcePage in executor.map(getPage, trainerHref):
+            #create a beautifulsoup object, return the object soup of type bs4.BeautifulSoup
+            soup = BeautifulSoup(sourcePage,features="html.parser")
+            #get trainers website link , return the object pageLink of type list
+            pageLink = soup.find('div', class_="tab-website").find('p', class_='purpleblock').find_all('a')
+            #condition to read element of pageLink
+            for link in pageLink:
+                #add the href of the link in trainerWebsite
+                trainerWebsite.append(link.get('href'))
+    #return trainerWebsite
+    return trainerWebsite
+
+def writeModel(trainersName,trainersWebsite):
+    #create the object that will contain the final output, return the object finalOutput of type list
+    finalOutput = []
+    for name,websiteLink in zip(trainersName,trainersWebsite):
+        #append name in finalOutput
+        finalOutput.append(("   Name   :"+("{}".center(10," "))).format(name))
+        #append websiteLink in finalOutput
+        finalOutput.append(("        Website-link   :"+("{}".center(10," "))+"\n\n").format(websiteLink))
+    #return finalOutput
+    return finalOutput
+
+def whriteInOuput(finalOutput):
+    """
+    Whrite the result in a file txt
+    """
+
+    os.chdir("D:/IIHT/Python/Project/NRPT all town trainer scrapper/caches")
+     #open text file, return  an object of type io.TextIOWrapper
+    with open("{}.txt".format(townName), "w") as writ:
+        #write each line in the object op, return an object of type int
+        writ.write('\n'.join(finalOutput) + "\n")
+
+towNameAndHref = getTownUrl()
+
+for townName,townHref in zip(towNameAndHref[0],towNameAndHref[1]):
+    paginationLinks = getPaginationLinks()
+    if paginationLinks == []:
+        pass
+    else:
+        trainerInfo = getTrainersUrl(paginationLinks)
+        trainersName = trainerInfo[1]
+        trainerHref = trainerInfo[0]
+        trainersWebsite = getTrainersWebsite(trainerHref)
+        finalOutput = writeModel(trainersName,trainersWebsite)
+        whriteInOuput(finalOutput)
